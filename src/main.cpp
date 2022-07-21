726c6c7e9c58c689ad6bdf62d44cb470b29536b2
Saurabh Singh <saurabh.s99100@gmail.com>
2022-07-21 14:10:20 +0530

    add basic example in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,23 @@
 #include "mem.h"
+#include "core.h"
+#include <memory>
 
 using namespace rvsim;
 
 int main(int argc, char ** argv) 
 {
-    Memory m(0x00000000, 64*1024, false);
+    std::unique_ptr<Memory> ram(new Memory(0x00000000, 64*1024, true));
+    std::unique_ptr<Memory> rom(new Memory(0x00000000, 64*1024, true));
+    
+    Core c(0);
+    c.attach_memdevice(rom.get());
+    c.attach_memdevice(ram.get());
 
+    c.step();
+    c.step();
+    c.step();
+    c.step();
+    c.step();
 
 
     return 0;
